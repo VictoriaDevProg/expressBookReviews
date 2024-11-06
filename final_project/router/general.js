604ab135d147b341460e562b70b3d6e8f26d2f8d
VictoriaDevProg <victoria.dev1001@gmail.com>
2024-11-05 22:09:05 -0300

    Task 2

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -19,7 +19,8 @@ public_users.get('/',function (req, res) {
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const booksByIsbn = req.params.isbn;
+  res.send(books[booksByIsbn])
  });
   
 // Get book details based on author
