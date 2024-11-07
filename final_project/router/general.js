167fed4c0b2fe1198f2b88de6252607c169c4747
VictoriaDevProg <victoria.dev1001@gmail.com>
2024-11-06 23:42:14 -0300

    Task 13

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -114,23 +114,38 @@ public_users.get('/author/:author', async (req, res) => {
 });
 
 
-// Get all books based on title
-public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  let ans = []
-  for(const [key, values] of Object.entries(books)){
-      const book = Object.entries(values);
-      for(let i = 0; i < book.length ; i++){
-          if(book[i][0] == 'title' && book[i][1] == req.params.title){
-              ans.push(books[key]);
-          }
-      }
-  }
-  if(ans.length == 0){
-      return res.status(300).json({message: "Title not found"});
-  }
-  res.send(ans);
-});
+// // Get all books based on title
+// public_users.get('/title/:title',function (req, res) {
+//   //Write your code here
+//   let ans = []
+//   for(const [key, values] of Object.entries(books)){
+//       const book = Object.entries(values);
+//       for(let i = 0; i < book.length ; i++){
+//           if(book[i][0] == 'title' && book[i][1] == req.params.title){
+//               ans.push(books[key]);
+//           }
+//       }
+//   }
+//   if(ans.length == 0){
+//       return res.status(300).json({message: "Title not found"});
+//   }
+//   res.send(ans);
+// });
+
+//Search by title using async for task 13
+public_users.get('/title/:title', async (req, res) => { 
+  try { 
+    const title = req.params.title; 
+    const matchingBooks = Object.values(books)
+    .filter(book => book.title === title); 
+    if (matchingBooks.length === 0) { 
+      return res.status(404).json({ message: "Title not found" });
+    } 
+      res.send(matchingBooks); 
+    } catch (error) { 
+        res.status(500).json({ message: "Internal Server Error" }); 
+    }
+})
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
